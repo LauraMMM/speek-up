215f9970ecd9f177f26c5bab22e82278f013ed9f
Laura Manescu <lcapatina@temenos.com>
2015-11-22 01:43:36 +0200

    Adding 3rd party

--- README.md
+++
@@ -1,4 +0,0 @@
-speekup
-=======
-
-A Symfony project created on November 21, 2015, 9:47 am.
--- src/Acme/SpeekUpMongoBundle/AcmeSpeekUpMongoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Acme\SpeekUpMongoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AcmeSpeekUpMongoBundle extends Bundle
-{
-}
